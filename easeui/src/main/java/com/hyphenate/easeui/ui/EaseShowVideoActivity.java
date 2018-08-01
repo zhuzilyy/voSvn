5be422c96fb56aaaa3a00ea3e1a1c432a1107e99
Administrator <287368384@qq,com>
2018-08-01 16:34:12 +0800

    保活提交

--- easeui/src/main/java/com/hyphenate/easeui/ui/EaseShowVideoActivity.java
+++ easeui/src/main/java/com/hyphenate/easeui/ui/EaseShowVideoActivity.java
@@ -34,7 +34,7 @@ public class EaseShowVideoActivity extends EaseBaseActivity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.ease_showvideo_activity);
