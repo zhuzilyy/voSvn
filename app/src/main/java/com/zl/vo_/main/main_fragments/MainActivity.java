3eefbb5f78de3f355d7ff18786beb145acce99d6
zhuzilyy <985085485@qq.com>
2018-08-10 13:31:11 +0800

    init

--- app/src/main/java/com/zl/vo_/main/main_fragments/MainActivity.java
+++ app/src/main/java/com/zl/vo_/main/main_fragments/MainActivity.java
@@ -1413,6 +1413,7 @@ public class MainActivity extends VoBaseActivity implements View.OnClickListener
         } catch (Exception e) {
 
         }
+        //ScreenReceiverUtil.
 
         //startActivity(new Intent(MainActivity.this, SplashActivityVo.class));
     }
