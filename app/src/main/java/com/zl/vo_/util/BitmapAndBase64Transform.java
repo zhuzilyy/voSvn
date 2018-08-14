f00027a61540404c765d9f59edfdc02cce98fc21
zhuzilyy <985085485@qq.com>
2018-08-14 23:10:58 +0800

    init

---
+++ app/src/main/java/com/zl/vo_/util/BitmapAndBase64Transform.java
@@ -0,0 +1,51 @@
+package com.zl.vo_.util;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class BitmapAndBase64Transform {
+	public static String Bitmap2StrByBase64(Bitmap bit) {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		bit.compress(CompressFormat.JPEG, 40, bos);
+		byte[] bytes = bos.toByteArray();
+		return Base64.encodeToString(bytes, Base64.DEFAULT);
+	}
+
+	public static String bitmapToBase64(Bitmap bitmap) {
+		String result = null;
+		ByteArrayOutputStream baos = null;
+		try {
+			if (bitmap != null) {
+				baos = new ByteArrayOutputStream();
+				bitmap.compress(CompressFormat.JPEG, 100, baos);
+				baos.flush();
+				baos.close();
+
+				byte[] bitmapBytes = baos.toByteArray();
+				result = Base64.encodeToString(bitmapBytes, Base64.DEFAULT);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (baos != null) {
+					baos.flush();
+					baos.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	//base64转化成bitmap
+	public static Bitmap base64ToBitmap(String base64Data) {
+		byte[] bytes = Base64.decode(base64Data, Base64.DEFAULT);
+		return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+	}
+}
